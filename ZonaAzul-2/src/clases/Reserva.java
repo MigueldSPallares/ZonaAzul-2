9b18ca741bf81528a57b5bb3abbda7e96702dbd9
Miguel <migueldspa2@gmail.com>
2020-04-17 13:15:06 +0200

    Estructura inicial

--- ZonaAzul-2/src/clases/Reserva.java
+++ ZonaAzul-2/src/clases/Reserva.java
@@ -1,5 +1,53 @@
 package clases;
 
 public class Reserva {
-
+	private String dni;
+	private String matricula;
+	private String fecha;
+	private String hora_inicio;
+	private String hora_fin;
+	
+	public Reserva(String dni, String matricula, String fecha, String hora_inicio, String hora_fin) {
+		super();
+		this.dni = dni;
+		this.matricula = matricula;
+		this.fecha = fecha;
+		this.hora_inicio = hora_inicio;
+		this.hora_fin = hora_fin;
+	}
+	@Override
+	public String toString() {
+		return "Reserva [dni=" + dni + ", matricula=" + matricula + ", fecha=" + fecha + ", hora_inicio=" + hora_inicio
+				+ ", hora_fin=" + hora_fin + "]";
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public String getHora_inicio() {
+		return hora_inicio;
+	}
+	public void setHora_inicio(String hora_inicio) {
+		this.hora_inicio = hora_inicio;
+	}
+	public String getHora_fin() {
+		return hora_fin;
+	}
+	public void setHora_fin(String hora_fin) {
+		this.hora_fin = hora_fin;
+	}
 }
--- ZonaAzul-2/src/clases/Usuario.java
+++ ZonaAzul-2/src/clases/Usuario.java
@@ -1,5 +1,48 @@
 package clases;
 
-public class Usuario {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Usuario {
+	private String dni;
+	private char[] password;
+	private ArrayList<String> vCoches;
+	private ArrayList<Reserva> vReservas;
+	
+	public Usuario(String dni, char[] password, ArrayList<String> vCoches, ArrayList<Reserva> vReservas) {
+		super();
+		this.dni = dni;
+		this.password = password;
+		this.vCoches = vCoches;
+		this.vReservas = vReservas;
+	}
+	@Override
+	public String toString() {
+		return "Usuario [dni=" + dni + ", password=" + Arrays.toString(password) + ", vCoches=" + vCoches
+				+ ", vReservas=" + vReservas + "]";
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public char[] getPassword() {
+		return password;
+	}
+	public void setPassword(char[] password) {
+		this.password = password;
+	}
+	public ArrayList<String> getvCoches() {
+		return vCoches;
+	}
+	public void setvCoches(ArrayList<String> vCoches) {
+		this.vCoches = vCoches;
+	}
+	public ArrayList<Reserva> getvReservas() {
+		return vReservas;
+	}
+	public void setvReservas(ArrayList<Reserva> vReservas) {
+		this.vReservas = vReservas;
+	}
 }
