ecf143dbdf1d7e2640f802c98527d7c264c487f2
Miguel <migueldspa2@gmail.com>
2020-04-21 23:21:47 +0200

    Comenzando con mi parte

--- ZonaAzul-2/src/clases/IOData.java
+++ ZonaAzul-2/src/clases/IOData.java
@@ -1,5 +1,20 @@
 package clases;
 
-public class IOData {
+import java.util.ArrayList;
 
+public class IOData {
+	public static void guardarNuevoUsuario() {
+		
+	}
+	public static ArrayList<Usuario> cargarNuevoUsuario() {
+		ArrayList<Usuario> vUsuario = new ArrayList<Usuario>();
+		return vUsuario;
+	}
+	public static void guardarNuevaReserva() {
+		
+	}
+	public static ArrayList<Reserva> cargarNuevaReserva() {
+		ArrayList<Reserva> vReserva = new ArrayList<Reserva>();
+		return vReserva;
+	}
 }
