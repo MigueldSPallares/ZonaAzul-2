29f1b1da2d93881f0ccf119056ca61b7aa46f191
Miguel <migueldspa2@gmail.com>
2020-05-03 23:37:33 +0200

    MI parte ya está terminada

--- ZonaAzul-2/src/clases/IOData.java
+++ ZonaAzul-2/src/clases/IOData.java
@@ -9,9 +9,10 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
+
 public class IOData {
 	public static void guardarNuevoUsuario(ArrayList<Usuario> vUsuario) {
-		File fichero = new File("Passwd.dat");
+		File fichero = new File("./Passwd.dat");
 		FileOutputStream fo = null;
 		ObjectOutputStream escribir = null;
 		if(!fichero.exists()) {
@@ -21,39 +22,142 @@ public class IOData {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-		try {
-			fo = new FileOutputStream(fichero);
-			escribir = new ObjectOutputStream(fo);
-			escribir.writeObject(vUsuario);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
+		}else {
 			try {
-				fo.close();
-				escribir.close();
+				fo = new FileOutputStream(fichero);
+				escribir = new ObjectOutputStream(fo);
+				escribir.writeObject(vUsuario);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			}finally {
+				try {
+					fo.close();
+					escribir.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
 	}
 	public static ArrayList<Usuario> cargarNuevoUsuario() {
 		ArrayList<Usuario> vUsuario = new ArrayList<Usuario>();
-		File fichero = new File("Passwd.dat");
+		File fichero = new File("./Passwd.dat");
 		FileInputStream fi = null;
 		ObjectInputStream leer = null;
+		if(!fichero.exists()) {
+			try {
+				fichero.createNewFile();
+				return vUsuario;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else {
+			try {
+				fi = new FileInputStream(fichero);
+				leer = new ObjectInputStream(leer);
+				
+				vUsuario = (ArrayList<Usuario>) leer.readObject();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				try {
+					if (fi!=null)
+						fi.close();
+					if (leer!=null)
+						leer.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 		return vUsuario;
 	}
-	public static void guardarNuevaReserva() {
-		
+	public static void guardarNuevaReserva(ArrayList<Reserva> vReserva) {
+		File fichero = new File("./reservasZonaAzul.dat");
+		FileOutputStream fo = null;
+		ObjectOutputStream escribir = null;
+		if(!fichero.exists()) {
+			try {
+				fichero.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else {
+			try {
+				fo = new FileOutputStream(fichero);
+				escribir = new ObjectOutputStream(fo);
+				escribir.writeObject(vReserva);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				try {
+					fo.close();
+					escribir.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 	public static ArrayList<Reserva> cargarNuevaReserva() {
 		ArrayList<Reserva> vReserva = new ArrayList<Reserva>();
+		File fichero = new File("./reservasZonaAzul.dat");
+		FileInputStream fi = null;
+		ObjectInputStream leer = null;
+		if(!fichero.exists()) {
+			try {
+				fichero.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else {
+			try {
+				fi = new FileInputStream(fichero);
+				leer = new ObjectInputStream(leer);
+				
+				vReserva = (ArrayList<Reserva>) leer.readObject();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				try {
+					if (fi!=null)
+						fi.close();
+					if (leer!=null)
+						leer.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 		return vReserva;
 	}
 }
--- ZonaAzul-2/src/login/Login.java
+++ ZonaAzul-2/src/login/Login.java
@@ -7,7 +7,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
+
+import clases.IOData;
+import clases.Usuario;
+import main.Main;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -15,7 +21,14 @@ import java.awt.event.ActionEvent;
 import javax.swing.ImageIcon;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.util.ArrayList;
+
 import javax.swing.JPasswordField;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Login extends JFrame {
 
@@ -25,6 +38,9 @@ public class Login extends JFrame {
 	private JTextField textDni;
 	private JPasswordField passwordField;
 	private JButton btnComprobar;
+	public ArrayList<Usuario> vUsuario = new ArrayList<Usuario>();
+	public boolean usuarioVacio;
+	private char passwordChar;
 
 	/**
 	 * Launch the application.
@@ -62,6 +78,8 @@ public class Login extends JFrame {
 		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\migue\\git\\ZonaAzul-2\\ZonaAzul-2\\src\\images\\admin.png"));
 		lblNewLabel.setBounds(91, 10, 243, 256);
 		contentPane.add(lblNewLabel);
+		lblNewLabel.setFocusable(true);
+		lblNewLabel.requestFocus();
 		
 		btnRegistrar = new JButton("Registrar nuevo usuario");
 		btnRegistrar.addMouseListener(new BtnRegistrarMouseListener());
@@ -69,18 +87,36 @@ public class Login extends JFrame {
 		contentPane.add(btnRegistrar);
 		
 		textDni = new JTextField();
+		textDni.addKeyListener(new TextDniKeyListener());
+		textDni.addFocusListener(new TextDniFocusListener());
+		textDni.setText("Usuario");
 		textDni.setBounds(140, 287, 146, 19);
 		contentPane.add(textDni);
 		textDni.setColumns(10);
 		
 		passwordField = new JPasswordField();
+		passwordField.setToolTipText("");
+		passwordChar = passwordField.getEchoChar();
+		passwordField.addFocusListener(new PasswordFieldFocusListener());
+		passwordField.addKeyListener(new PasswordFieldKeyListener());
 		passwordField.setBounds(140, 316, 146, 19);
+		passwordField.setText("Contraseña");
+		passwordField.setEchoChar((char)0);
 		contentPane.add(passwordField);
 		
+		
 		btnComprobar = new JButton("Comprobar usuario");
+		btnComprobar.addMouseListener(new BtnComprobarMouseListener());
 		btnComprobar.setBounds(140, 355, 146, 21);
 		contentPane.add(btnComprobar);
 		btnComprobar.setEnabled(false);
+		usuarioVacio = false;
+		try {
+			vUsuario = IOData.cargarNuevoUsuario();
+			usuarioVacio = true;
+		}catch (NullPointerException e){
+			
+		}
 	}
 	private class BtnRegistrarMouseListener extends MouseAdapter {
 		@Override
@@ -88,6 +124,62 @@ public class Login extends JFrame {
 			Register r = new Register();
 			r.setVisible(true);
 			dispose();
+		}
+	}
+	private class TextDniFocusListener extends FocusAdapter {
+		@Override
+		public void focusGained(FocusEvent e) {
+			if(textDni.getText().equalsIgnoreCase("Usuario")) {
+				textDni.setText("");
+			}
+		}
+	}
+	private class BtnComprobarMouseListener extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if(!usuarioVacio) {
+				for (Usuario usuario : vUsuario) {
+					if(textDni.getText().equalsIgnoreCase(usuario.getDni())&&passwordField.getText().equalsIgnoreCase(usuario.getPassword())) {
+						Main m = new Main(usuario.getDni());
+						m.setVisible(true);
+						dispose();
+					}else {
+						JOptionPane.showMessageDialog(null, "Usuario Incorrecto");
+						textDni.setText("Usuario");
+						passwordField.setText("Contraseña");
+						passwordField.setEchoChar((char)0);
+					}
+				}
+			}
+		}
+	}
+	private class TextDniKeyListener extends KeyAdapter {
+		@Override
+		public void keyReleased(KeyEvent e) {
+			comprobarRellenado();
+		}
+	}
+	private class PasswordFieldKeyListener extends KeyAdapter {
+		@Override
+		public void keyReleased(KeyEvent e) {
+			comprobarRellenado();
+		}
+	}
+	private class PasswordFieldFocusListener extends FocusAdapter {
+		@Override
+		public void focusGained(FocusEvent e) {
+			if(passwordField.getText().equalsIgnoreCase("Contraseña")) {
+				passwordField.setText("");
+				passwordField.setEchoChar(passwordChar);
+			}
+		}
+	}
+	private void comprobarRellenado() {
+		if(textDni.getText().equalsIgnoreCase("Usuario") || textDni.getText().equalsIgnoreCase("") || 
+				passwordField.getText().equalsIgnoreCase("Contraseña") || passwordField.getText().equalsIgnoreCase("")) {
+			btnComprobar.setEnabled(false);
+		}else {
+			btnComprobar.setEnabled(true);
 		}
 	}
 }
